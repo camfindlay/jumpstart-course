e2456a173b96b62bfb3b3ac9a09834de9efe542e
Cam Findlay <cam@silverstripe.com>
2016-01-26 12:27:50 +1300

    4.3 Administration of DataObjects using GridField

--- mysite/code/StaffHolder.php
+++ mysite/code/StaffHolder.php
@@ -5,6 +5,32 @@ class StaffHolder extends Page {
 
     private static $allowed_children = array('StaffPage');
 
+    public function getCMSFields(){
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab(
+            'Root.Departments',
+            GridField::create(
+                'Department',
+                'Departments',
+                Department::get()->sort('Title'),
+                GridFieldConfig_RecordEditor::create()
+            )
+        );
+
+        $fields->addFieldToTab(
+            'Root.Roles',
+            GridField::create(
+                'Role',
+                'Roles',
+                Role::get()->sort('Title'),
+                GridFieldConfig_RecordEditor::create()
+            )
+        );
+
+        return $fields;
+    }
+
 }
 
 class StaffHolder_Controller extends Page_Controller { }
